0cd450a556266aac4882dab8dd384cb6e6e0ee0b
heyyitzaashi02 <46236065+heyyitzaashi02@users.noreply.github.com>
2018-12-29 14:35:56 +0530

    Q-18

---
+++ Untitled17.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# program to calculate the x to the power y without using standard function
+num=int(input("enter a number"))
+num2=int(input("enter 2nd number"))
+a=1
+for num2 in range (1,num2+1):
+    a=(a*num)
+print(a)
+        
+
+
+# In[ ]:
+
+
+
+
