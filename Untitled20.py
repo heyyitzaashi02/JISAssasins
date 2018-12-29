05329d3c7d9bc04887e617261c2d30a67ad57f8d
heyyitzaashi02 <46236065+heyyitzaashi02@users.noreply.github.com>
2018-12-29 15:08:44 +0530

    Q-14

---
+++ Untitled20.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# program to accept 'n' no from the user and store these no into an array an count the no of occurrence of each no
+str=input("Enter the string")
+char=input("Enter the character whose number of occurences is to be found in the string")
+c=str.count(char)
+print("Number of occurences of {} is {}".format(char,c))
+
+
+# In[ ]:
+
+
+
+
