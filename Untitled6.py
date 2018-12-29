0d3d42caaa1557bf40fe70d3c4a80821050d00ae
heyyitzaashi02 <46236065+heyyitzaashi02@users.noreply.github.com>
2018-12-29 11:47:55 +0530

    Q-20

---
+++ Untitled6.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# write program to accept string from the user and replace all accurences of character 
+
+string=input("Enter string:")
+string=string.replace('a','*')
+
+print("Modified string:")
+print(string)
+
+
+# In[ ]:
+
+
+
+
