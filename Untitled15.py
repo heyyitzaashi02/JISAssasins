189a31b7e67ee7cba847ab19a820c0efdf3e03a2
heyyitzaashi02 <46236065+heyyitzaashi02@users.noreply.github.com>
2018-12-29 14:18:46 +0530

    Q-24

---
+++ Untitled15.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# program to display the multipication table
+
+
+num = 12
+
+# use for loop to iterate 10 times
+for i in range(1, 11):
+   print(num,'x',i,'=',num*i)
+
+
+# In[ ]:
+
+
+
+
