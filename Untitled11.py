a792abb1ceb58b206abc89589f81f7e3cc8c004c
heyyitzaashi02 <46236065+heyyitzaashi02@users.noreply.github.com>
2018-12-29 12:45:32 +0530

    Q-12

---
+++ Untitled11.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# program to accept a sentence from the user and reverse 
+
+msg="you are a good girl"
+
+words=msg.split(" ")
+words.reverse()
+print(' '.join(words))
+
+
+# In[ ]:
+
+
+
+
