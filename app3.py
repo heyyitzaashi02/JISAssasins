aa9ef8f11697cc8c191db364506e84fcde8e044d
heyyitzaashi02 <46236065+heyyitzaashi02@users.noreply.github.com>
2018-12-29 11:18:13 +0530

    Q-7

---
+++ app3.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# taking the number as string
+num = input("enter a number: ")
+sum = 0
+
+for i in num:
+    sum += int(i)
+    
+print("The sum of digits of number is",sum)
+
+
+# In[ ]:
+
+
+
+
