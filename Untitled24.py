fa8e5bae95624479732b69b3aeb15b2f8f479dac
heyyitzaashi02 <46236065+heyyitzaashi02@users.noreply.github.com>
2018-12-30 21:56:37 +0530

    Q-15

---
+++ Untitled24.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+#QSN15 program to calculate X to the power y + z(by amisha singh)
+
+x=int(input("Enter the number"))
+y=int(input("Enter the number"))
+z=int(input("Enter the number"))
+c=x**(y+z)
+print(c)
+
+
+# In[ ]:
+
+
+
+
