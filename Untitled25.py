fb41fcacba78141c30204af721b4669f74e3e2e5
heyyitzaashi02 <46236065+heyyitzaashi02@users.noreply.github.com>
2018-12-30 22:22:49 +0530

    Q-9

---
+++ Untitled25.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+#(Q-9) program to calculate the sum of first digit of the given number(by amisha singh)
+class Sum:
+    def cont(self,n):
+        self.z=int(n[0])+ int(n[-1])
+        print("sum of the first and last digits is ",self.z)
+calu=Sum()
+y=input("Enter the number \n")
+calu.cont(y)
+
+
+# In[ ]:
+
+
+
+
