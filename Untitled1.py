58c880732cf66797942ce8ec0316ee90d57cba25
heyyitzaashi02 <46236065+heyyitzaashi02@users.noreply.github.com>
2018-12-30 20:33:24 +0530

    Q-16

---
+++ Untitled1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#program to accept character and display ASCII
+
+# Change this value for a different result
+c = 'p'
+
+# Uncomment to take character from user
+#c = input("Enter a character: ")
+
+print("The ASCII value of '" + c + "' is",ord(c))
+
